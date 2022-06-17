581faa882ed7fec6e2e925006d85a6734a7f3f92
caulok <ramicaulo@gmail.com>
2022-06-17 16:44:39 -0300

    Entrega de desafío complementario / Ciclos

---
+++ js/index.js
@@ -0,0 +1,7 @@
+let numero = parseFloat(prompt(`Ingrese debajo un número\n\nEn la consola mostraremos resultados de multiplicaciones hasta que el número ingresado lo haga por si mismo`));
+
+for (let i = 0; i <= numero; i++) {
+    let multiplicacion = i*numero;
+
+    console.log(`${numero} x ${i} = ${multiplicacion}`)
+}
